ff30892c89dd7f7bf5c1f5e203b487d7d458be3c
Ahnaf Tazwar Ayub <ahnafayub@Tazs-MacBook-Air-M1-2.local>
2022-04-04 17:08:18 -0700

    Updated double spot case

--- parking_spot.py
+++ parking_spot.py
@@ -57,7 +57,7 @@ class SingleSpot(ParkingSpot):
         self.capacity = 1
         
         self.lam_depart = 1/np.random.randint(10, 20+1) # 1 car leaves every A...X minutes
-        self.lam_arrive = 1/np.random.randint(20, 27+1) # 1 car arrives every A...X minutes
+        self.lam_arrive = 1/np.random.randint(20, 25+1) # 1 car arrives every A...X minutes
         self.form_models()
 
 class DoubleSpot(ParkingSpot):
@@ -65,6 +65,6 @@ class DoubleSpot(ParkingSpot):
         super().__init__(location, geo_area, rate, time_limit)
         self.capacity = 2
         
-        self.lam_depart = 1/np.random.randint(10, 15) # 1 car leaves every A...X minutes
-        self.lam_arrive = 1/np.random.randint(20, 25) # 1 car arrives every A...X minutes
+        self.lam_depart = 1/np.random.randint(10, 15+1) # 1 car leaves every A...X minutes
+        self.lam_arrive = 1/np.random.randint(20, 25+1) # 1 car arrives every A...X minutes
         self.form_models()
--- utils/processor.py
+++ utils/processor.py
@@ -24,8 +24,11 @@ class Processor(object):
     def load_parking_meters_data(self):
         self.df = pd.read_csv(self.datapath, sep=';')
         print(self.df.shape)
-        self.df = self.df.loc[self.df.METERHEAD.isin(['Single', 'Double'])]
+        self.df = self.df.loc[self.df.METERHEAD.isin(['Single','Twin'])]
+        if self.df.Geom.isna().any():
+            self.df = self.df.loc[~self.df.Geom.isna()]
         self.df['latln'] = self.df['Geom'].apply(self.__get_latln)
+        self.__merge_twins()
         self.df['dist_from_dest'] = self.df['latln'].apply(self.__compute_distances_to_dest)
         print(self.df.shape)
 
@@ -36,10 +39,57 @@ class Processor(object):
     def __compute_distances_to_dest(self, row):
         return gd.geodesic(self.dest, row).km
 
+    def __merge_twins(self):
+        #Find rest
+        df_no_twins = self.df.loc[~(self.df.METERHEAD=='Twin')]
+        df_no_twins.reset_index(inplace=True)
+        #Find all twins
+        twins = self.df.loc[self.df.METERHEAD=='Twin']
+        twins.reset_index(inplace=True)
+        #Make sure we are finding unique spots
+        seen_twins = set()
+        for i, row in twins.iterrows():
+            #find the match
+            pair = tuple(np.sort(np.where(twins.latln == row['latln'])[0]))
+            if len(pair) != 2:
+                continue
+            if pair not in seen_twins:
+                assert i == pair[0] #Current one is the first
+                seen_twins.add(pair)
+            else:
+                continue
+            # print(pair)
+            check_columns = ['METERHEAD', 'Geo Local Area', 'latln']
+            meter1, meter2 = twins.loc[pair[0]], twins.loc[pair[1]]
+            for col in check_columns:
+                v1, v2 = meter1[col], meter2[col]
+                if not pd.isnull(v1) and not pd.isnull(v2):
+                    assert v1 == v2
+        print(f'Original #of separate twins: {len(twins)}')
+        print(f'% retained after merging: {100*len(seen_twins)/len(twins)}')
+    
+        '''
+        Use Twins
+        for each pair, use first one's info to create a double spot
+        '''
+        merged_twins = []
+        for pair in seen_twins:
+            first, second = pair
+            first_meter = twins.loc[first]
+            merged_meter = first_meter
+            merged_meter['METERID'] = (twins.loc[first]['METERID'], twins.loc[second]['METERID'])
+            merged_twins.append(first_meter)
+        merged_twins = pd.DataFrame(merged_twins)
+        merged_twins.reset_index(inplace=True)
+
+        self.df = pd.concat([df_no_twins, merged_twins])
+        self.df.reset_index(inplace=True, drop=True)
+        print(f'*** Data shape after merge: {self.df.shape} ***')
+
     def get_nearest(self):
         nearest_locs  = self.df.dist_from_dest.sort_values().index 
         nearest = self.df.loc[nearest_locs].head(n=self.topK)
-        nearest.reset_index(inplace=True)
+        nearest.reset_index(inplace=True, drop=True)
         return nearest
 
     def form_variables_dict(self):
@@ -61,8 +111,8 @@ class Processor(object):
                 spot = SingleSpot(location = {'lat':location[0], 'lon':location[1]},
                                 geo_area = meter['Geo Local Area'], 
                                 rate = rate, time_limit=time_limit)
-            elif meter['METERHEAD'] == 'Double':
-                print("******************** DOUBLE **********************")
+            elif meter['METERHEAD'] == 'Twin':
+                print("******************** Twin **********************")
                 spot = DoubleSpot(location = {'lat':location[0], 'lon':location[1]},
                                 geo_area = meter['Geo Local Area'], 
                                 rate = rate, time_limit=time_limit)
@@ -124,4 +174,3 @@ class Processor(object):
                 print(f"Error with {i}")
                 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         return variables
-        
